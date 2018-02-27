a3ce06f35657017f6bb9636cf0f6ff57f7bd20e5
Adam Miner <miner.adam1@hotmail.com>
2018-02-27 13:57:43 -0700

    Adding Game Characters

---
+++ assets/javascript/app.js
@@ -0,0 +1,5 @@
+$(document).ready(() => {
+  var correct;
+  var incorrect;
+  var characters = ['Ezio Auditore', 'Shepard', 'Master Chief', 'MegaMan', 'Link', 'Zelda', 'Mario', 'Luigi', 'Illusive Man', 'Saren', 'Collector', 'Reaper', 'Edward Kenway', 'Connor Kenway', 'Bayek', 'Julius Ceasar', 'Cleopatra', 'Doom Marine', 'Altaïr', 'Evie Frye', 'Jacob Frye', 'Shay Cormac', 'Arno Dorian', 'Haytham Kenway', 'Bowser', 'Pit', 'Leonardo da Vinci', 'Cesare Borgia', 'Rodrigo Borgia', 'Lucrezia Borgia', 'Palutena', 'Pac-Man', 'Ms. Pac-Man', 'Inky', 'Pinky', 'Blinky', 'Clyde', 'Queen Victoria'];
+});
