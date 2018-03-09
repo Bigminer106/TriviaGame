327cfbb6bedfc507c7ae88f05971518d69ee7299
Adam Miner <miner.adam1@hotmail.com>
2018-03-08 20:56:00 -0700

    Answers and Questions Created

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,104 @@
 $(document).ready(() => {
   var score = 0;
-  $('.correct').on('click', () => {
-    $('.correct').css('background-color', 'green');
+
+  $('.correct1').on('click', () => {
+    $('.correct1').css('background-color', 'green');
     score++;
   });
-  $('.incorrect').on('click', () => {
-    $('.incorrect').css('background-color', 'red');
+
+  $('.correct2').on('click', () => {
+    $('.correct2').css('background-color', 'green');
+    score++;
   });
+
+  $('.correct3').on('click', () => {
+    $('.correct3').css('background-color', 'green');
+    score++;
+  });
+
+  $('.correct4').on('click', () => {
+    $('.correct4').css('background-color', 'green');
+    score++;
+  });
+
+  $('.correct5').on('click', () => {
+    $('.correct5').css('background-color', 'green');
+    score++;
+  });
+
+  $('.correct6').on('click', () => {
+    $('.correct6').css('background-color', 'green');
+    score++;
+  });
+
+  $('.correct7').on('click', () => {
+    $('.correct7').css('background-color', 'green');
+    score++;
+  });
+
+  $('.correct8').on('click', () => {
+    $('.correct8').css('background-color', 'green');
+    score++;
+  });
+
+  $('.correct9').on('click', () => {
+    $('.correct9').css('background-color', 'green');
+    score++;
+  });
+
+  $('.correct10').on('click', () => {
+    score++;
+    $('.correct10').css('background-color', 'green');
+  });
+
+  $('.incorrect1').on('click', () => {
+    $('.incorrect1').css('background-color', 'red');
+    $('.correct1').css('background-color', 'green');
+  });
+
+  $('.incorrect2').on('click', () => {
+    $('.incorrect2').css('background-color', 'red');
+    $('.correct2').css('background-color', 'green');
+  });
+
+  $('.incorrect3').on('click', () => {
+    $('.incorrect3').css('background-color', 'red');
+    $('.correct3').css('background-color', 'green');
+  });
+
+  $('.incorrect4').on('click', () => {
+    $('.incorrect4').css('background-color', 'red');
+    $('.correct4').css('background-color', 'green');
+  });
+
+  $('.incorrect5').on('click', () => {
+    $('.incorrect5').css('background-color', 'red');
+    $('.correct5').css('background-color', 'green');
+  });
+
+  $('.incorrect6').on('click', () => {
+    $('.incorrect6').css('background-color', 'red');
+    $('.correct6').css('background-color', 'green');
+  });
+
+  $('.incorrect7').on('click', () => {
+    $('.incorrect7').css('background-color', 'red');
+    $('.correct7').css('background-color', 'green');
+  });
+
+  $('.incorrect8').on('click', () => {
+    $('.incorrect8').css('background-color', 'red');
+    $('.correct8').css('background-color', 'green');
+  });
+
+  $('.incorrect9').on('click', () => {
+    $('.incorrect9').css('background-color', 'red');
+    $('.correct9').css('background-color', 'green');
+  });
+
+  $('.incorrect10').on('click', () => {
+    $('.incorrect10').css('background-color', 'red');
+    $('.correct10').css('background-color', 'green');
+  });
+
 });
